ea59e40e1fd9ddbcccfbe5db30b8a32ebbe6332e
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-26 14:43:40 -0700

    Add constants

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+exports.CODE = require("./code.constants");
+exports.ROLE = require("./role.constants");
+exports.STATUS = require("./status.constants");
+
+exports.OP_DAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
