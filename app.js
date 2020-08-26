ffa96df5d49e3e4d32476681e6f5ed2a10f38956
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-08-26 14:46:13 -0700

    Add app and server files

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+// const hpp = require('hpp');
+
+const controller = require("./controllers");
+const routes = require("./routes");
+const { AppError } = require("./utils");
+const { CODE } = require("./constants");
+
+const app = express();
+
+// 1) MIDDLEWARES
+// Set security headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Limit request from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP. Please try again in an hour.",
+});
+app.use("/api", limiter);
+
+// Body parser, sets req.body with data from body of request
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevents parameter pollution
+// app.use(
+//   hpp({
+//     whitelist: WHITELIST.PARAMS,
+//   })
+// );
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// 3) ROUTES
+app.use("/api/v1/user", routes.user);
+app.use("/api/v1/stand", routes.stand);
+
+app.all("*", (req, res, next) => {
+  const err = new AppError(
+    `${req.originalUrl} is not a valid endpoint`,
+    CODE.NOT_FOUND
+  );
+  next(err);
+});
+
+app.use(controller.error.errorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
+
+process.on("uncaughtException", (err) => {
+  console.log(
+    `🧨💥 UNHANDLED REJECTION 💥🧨\n  ${err.name}: ${err.message}\n  Shutting down...`
+  );
+  process.exit(1);
+});
+
+const app = require("./app");
+
+const DB = process.env.MONGO_URI.replace("<PWD>", process.env.MONGO_PWD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database connection successful.");
+  });
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(
+    `🧨💥 UNHANDLED REJECTION 💥🧨\n  ${err.name}: ${err.message}\n  Shutting down...`
+  );
+  server.close(() => {
+    process.exit(1);
+  });
+});
