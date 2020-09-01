dfe2e18e05e7e74136855fa16110498d59e2557f
Jorge L. Garcia <jlg.sftdev@gmail.com>
2020-09-01 14:38:21 -0700

    Modify user routes authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -13,30 +13,43 @@ router.post("/login", controller.auth.login);
 // router.post("/forgot-password", controller.user.forgotPassword);
 // router.post("/reset-password", controller.user.resetPassword);
 
-// All routes below this comment require authentication
+// All routes below this line require authentication
 router.use(controller.auth.protect);
-router
-  .route("/")
-  .get(controller.auth.restrictTo(ROLE.ADMIN), controller.user.getAllUsers);
 
+// User root route
+router.route("/").get(controller.user.getAllUsers);
+
+// Current logged in user route
 router
   .route("/me")
   .get(controller.user.getMe, controller.user.getUser)
   .patch(controller.user.updateMe)
   .delete(controller.user.deleteMe);
 
-router.use(controller.auth.restrictTo(ROLE.USER, ROLE.ADMIN));
+// Specific user route
 router.route("/:id").get(controller.user.getUser);
 
 // Review routes
 router
   .route("/:id/review")
   .get(controller.review.getAllReviews)
-  .post(controller.auth.idBelongsToUser, controller.review.createReview);
+  .post(
+    controller.auth.restrictTo(ROLE.ADMIN, ROLE.USER),
+    controller.auth.idBelongsToUser,
+    controller.review.createReview
+  );
 router
   .route("/:id/review/:reviewId")
-  .patch(controller.auth.idBelongsToUser, controller.review.updateReview)
-  .delete(controller.auth.idBelongsToUser, controller.review.deleteReview);
+  .patch(
+    controller.auth.restrictTo(ROLE.ADMIN, ROLE.USER),
+    controller.auth.idBelongsToUser,
+    controller.review.updateReview
+  )
+  .delete(
+    controller.auth.restrictTo(ROLE.ADMIN, ROLE.USER),
+    controller.auth.idBelongsToUser,
+    controller.review.deleteReview
+  );
 
 // Favorite routes
 router
